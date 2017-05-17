390820ed8144495ed9950296dd9d298ec0025082
lihongcheng <1519492056@qq.com>
2017-05-17 17:55:02 +0800

    SQL

---
+++ database/migrations/2017_05_17_064824_create_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->increments('id')->AUTO_INCREMENT();
+            $table->string('openid');
+            $table->string('tel');
+            $table->string('name');
+            $table->string('zhiwei');
+            $table->string('email')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
