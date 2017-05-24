46c7c92b692c64b3bf73585218e7ef811142fa69
lihongcheng <1519492056@qq.com>
2017-05-25 01:07:35 +0800

    telok

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -19,7 +19,7 @@ class WechatController extends Controller
                 case 'event':
                     //return '收到事件消息';
                     $yuangong = Teluser::where('openid', $message->FromUserName)->firstOrFail();
-                    return $yuangong->name;
+                    return $yuangong->name.':'.$yuangong->tel;
                     break;
                 case 'text':
                     return $userApi->get($message->FromUserName)->nickname;
