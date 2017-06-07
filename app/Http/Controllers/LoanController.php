84c01e224f9b5e971412ca75e2c4b658840ee1f0
lihongcheng <1519492056@qq.com>
2017-06-08 01:08:25 +0800

    order

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -30,11 +30,11 @@ class LoanController extends Controller
 //方式一：直接使用file_put_contents
             $tmp_file = date('mdHis') . mt_rand(100, 999). '.' .$type;
             $tmp_file =  'uploads/' . $tmp_file;
-            file_put_contents($tmp_file, $img); //可以直接将文件流保存为本地图片
+//            file_put_contents($tmp_file, $img); //可以直接将文件流保存为本地图片
 
 //方式二：先转换为GD图像资源，再生成文件或显示输出
- //           $im = imagecreatefromstring($img); //resource(2) of type (gd) 图像资源
- //           imagejpeg ($im, $tmp_file); //图像流（image）以 JPEG 格式输出到标准输出(浏览器或者文件)
+            $im = imagecreatefromstring($img); //resource(2) of type (gd) 图像资源
+            imagejpeg ($im, $tmp_file); //图像流（image）以 JPEG 格式输出到标准输出(浏览器或者文件)
 
 //或者使用AliOSS上传
 //$url = OSS::upload($img, $type);
--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -1,79 +1,98 @@
 @extends('master')
 @section('title','申请')
 @section('content')
-    <div class="weui_tab">
-        <div class="weui_navbar">
-            <a href="#tab1" class="weui_navbar_item weui_bar_item_on">
-                选项一
-            </a>
-            <a href="#tab2" class="weui_navbar_item">
-                选项二
-            </a>
-            <a href="#tab3" class="weui_navbar_item">
-                选项三
-            </a>
-        </div>
-        <!--<div class="weui_tab_bd">
-            <p>主体内容放这里</p>
-        </div>-->
+    <style>
+        h4 {
+            font-weight: 400;
+            font-size: 17px;
+            width: auto;
+            overflow: hidden;
+            text-overflow: ellipsis;//文字超过盒子宽度显示省略符号
+        white-space: nowrap;//文本不会换行，文本会在在同一行上继续，直到遇到 <br> 标签为止
+            /*不明白微信为毛写那么多和换行相关的属性*/
+        word-wrap: normal;
+            word-wrap: break-word;//允许长单词换行到下一行
+        word-break: break-all;//使用浏览器默认的换行规则
 
-        <style>
-            .clear{clear: both;}
-            .margin_50{margin-top: 52px;}
-            .pad_6{padding-left: 6px;padding-right: 6px;}
-            .mag_item{width: 100%;height: auto;}
-            .mag_item img{width: 100%;height: auto;border: 0;}
-        </style>
-        <div class="clear margin_50"></div>
-        <div class="weui_tab_bd">
-            <div id="tab1" class="weui_tab_bd_item weui_tab_bd_item_active pad_6">
-                <div class="mag_item">
-                    <img src="images/coubg.png" >
-                    <p>期待更多的笔记可以和大家分享</p>
-                </div>
+        }
 
-                <div class="mag_item">
-                    <img src="images/coubg.png">
-                    <p>希望可以拜读大家的作品 为榭</p>
-                </div>
-
-                <div class="mag_item">
-                    <img src="images/coubg.png" >
-                    <p>其实只要你不输给自己 就可以让自己活得更加充实和快乐</p>
-                </div>
+        p {
+            color: #999999;
+            font-size: 13px;
+            line-height: 1.2;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            display: -webkit-box;
+            -webkit-box-orient: vertical;
+            -webkit-line-clamp: 2;
+        }
+    </style>
+    <link rel="stylesheet" href="{{asset('css/weui2.css')}}">
+    <div class="weui_tab " style="height:44px;" id="tab1"><!--tab-fixed添加顶部-->
+        <div class="weui_navbar" style="height:44px;">
+            <div class="weui_navbar_item a">
+                已发货
             </div>
-            <div id="tab2" class="weui_tab_bd_item">
-                <h2 class="doc-head">希望可以拜读大家的作品 为榭</h2>
+            <div class="weui_navbar_item b">
+                未发货
             </div>
-            <div id="tab3" class="weui_tab_bd_item">
-                <h3 class="doc-head">其实只要你不输给自己 就可以让自己活得更加充实和快乐</h3>
+            <div class="weui_navbar_item c">
+                全部订单
             </div>
-
+            <div class="weui_navbar_item d">
+                全部订单
+            </div>
+            <div class="weui_navbar_item e">
+                全部订单
+            </div>
         </div>
     </div>
+
+    <div class="weui-flex a">
+        <div class="weui-flex-item" ><div class="placeholder">weui</div></div>
+    </div>
+    <div class="weui-flex">
+        <div class="weui-flex-item"><div class="placeholder">weui</div></div>
+        <div class="weui-flex-item">
+            <div class="media_bd">
+                <a href="1234">     <h4 class="media_title">456789123</h4>
+                <p class="media_desc">作者:1234  发布时间:1234</p></a>
+            </div></div>
+    </div>
+    <div class="weui-flex a">
+        <div class="weui-flex-item b"><div class="placeholder">weui</div></div></a>
+        <div class="weui-flex-item a"><div class="placeholder">weui</div></div>
+        <div class="weui-flex-item a"><div class="placeholder">weui</div></div>
+    </div>
+    <div class="weui-flex">
+        <div class="weui-flex-item 1"><div class="placeholder">weui</div></div>
+        <div class="weui-flex-item 1"><div class="placeholder">weui</div></div>
+        <div class="weui-flex-item"><div class="placeholder">weui</div></div>
+        <div class="weui-flex-item a"><div class="placeholder">55555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555</div></div>
+    </div>
+
+
 
 @endsection
 
 @section('js')
+    $('#tab1').tab({defaultIndex:0,activeClass:"tab-green"});
 
-    $(function() {
 
-    var i=0;
-    $(".weui_navbar a").bind("click", function(){
 
-    //css操作
-    {{--alert(i++);--}}
-    //操作导航栏
-    $(".weui_bar_item_on").removeClass('weui_bar_item_on');
-    //console.log($(this).find("a"));
-    $(this).addClass("weui_bar_item_on");
+    $(function(){
+    $('.weui_navbar_item').on('click', function () {
+    $(".weui-flex-item.a").hide(0);
+    $(this).addClass('weui-bar__item_on').siblings('.weui-bar__item_on').removeClass('weui-bar__item_on');
+    });
+    });
 
-    //操作内容切换
-    $(".weui_tab_bd .weui_tab_bd_item_active").removeClass('weui_tab_bd_item_active');
-    var data_toggle =jQuery(this).attr("href");
-    $(data_toggle).addClass("weui_tab_bd_item_active");
-    // $(this).addClass("weui_tab_bd_item_active");
-
+    $(document).ready(function(){
+    $(".btn1").click(function(){
+    $(".page").hide(0);
+    });
+    $(".btn2").click(function(){
+    $(".page").show(0);
     });
     });
 
