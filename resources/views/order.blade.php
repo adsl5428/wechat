4df2b8db438bb4f9d6b3f4b7fd17507e6b837eaa
lihongcheng <1519492056@qq.com>
2017-06-10 18:30:10 +0800

    order

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -11,45 +11,42 @@
             text-align: center;
         }
 
-        h4 {
-            font-weight: 400;
-            font-size: 17px;
-            width: auto;
-            overflow: hidden;
-            text-overflow: ellipsis;//文字超过盒子宽度显示省略符号
-        white-space: nowrap;//文本不会换行，文本会在在同一行上继续，直到遇到 <br> 标签为止
-            /*不明白微信为毛写那么多和换行相关的属性*/
-        word-wrap: normal;
-            word-wrap: break-word;//允许长单词换行到下一行
-        word-break: break-all;//使用浏览器默认的换行规则
-        }
+        /*h4 {*/
+            /*font-weight: 400;*/
+            /*font-size: 17px;*/
+            /*width: auto;*/
+            /*overflow: hidden;*/
+            /*text-overflow: ellipsis;//文字超过盒子宽度显示省略符号*/
+        /*white-space: nowrap;//文本不会换行，文本会在在同一行上继续，直到遇到 <br> 标签为止*/
+            /*!*不明白微信为毛写那么多和换行相关的属性*!*/
+        /*word-wrap: normal;*/
+            /*word-wrap: break-word;//允许长单词换行到下一行*/
+        /*word-break: break-all;//使用浏览器默认的换行规则*/
+        /*}*/
 
-        p {
-            color: #999999;
-            font-size: 13px;
-            line-height: 1.2;
-            overflow: hidden;
-            text-overflow: ellipsis;
-            display: -webkit-box;
-            -webkit-box-orient: vertical;
-            -webkit-line-clamp: 2;
-        }
-        .f
-        {
-            height: 5em;
-        }
+        /*p {*/
+            /*color: #999999;*/
+            /*font-size: 13px;*/
+            /*line-height: 1.2;*/
+            /*overflow: hidden;*/
+            /*text-overflow: ellipsis;*/
+            /*display: -webkit-box;*/
+            /*-webkit-box-orient: vertical;*/
+            /*-webkit-line-clamp: 2;*/
+        /*}*/
+
     </style>
     <link rel="stylesheet" href="{{asset('css/weui2.css')}}">
 
     <div class="weui_tab " style="height:44px;" id="tab1"><!--tab-fixed添加顶部-->
         <div class="weui_navbar" style="height:44px;">
             <div class="weui_navbar_item a">
-                已发货
+                全部订单
             </div>
             <div class="weui_navbar_item b">
-                未发货
+                初审
             </div>
-            <div class="weui_navbar_item">
+            <div class="weui_navbar_item c">
                 全部订单
             </div>
             <div class="weui_navbar_item d">
@@ -74,7 +71,7 @@
         </div>
     </div>
 
-
+{{--名字 项目  金额 进度 --}}
     <div class="weui-flex a">
         <div class="weui-flex-item"><div class="placeholder">weui</div></div>
         <div class="weui-flex-item"><div class="placeholder">weui</div></div>
@@ -82,7 +79,7 @@
     <div class="weui-flex">
         <div class="weui-flex-item"><div class="placeholder">weui</div></div>
         <div class="weui-flex-item"><div class="placeholder">weui</div></div>
-        <div class="weui-flex-item"><div class="placeholder">55555555555555555555555555555555555555</div></div>
+        <div class="weui-flex-item"><div class="placeholder">563</div></div>
     </div>
 
 
