766b99207035c3486b289d7916e546ae224e3836
lihongcheng <1519492056@qq.com>
2017-06-21 15:20:44 +0800

    ffff

--- config/wechat.php
+++ config/wechat.php
@@ -85,5 +85,5 @@ return [
 //         "city" =>"北京",
 //         "country" =>"中国",
 //         "headimgurl" => "http://wx.qlogo.cn/mmopen/C2rEUskXQiblFYMUl9O0G05Q6pKibg7V1WpHX6CIQaic824apriabJw4r6EWxziaSt5BATrlbx1GVzwW2qjUCqtYpDvIJLjKgP1ug/0",
-//     ],
+//     ], 
 ];
