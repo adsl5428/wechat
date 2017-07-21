04b3e4ceef99254a4234737a78d126e375b7af27
lihongcheng <1519492056@qq.com>
2017-07-22 04:00:34 +0800

    newmenu2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -33,7 +33,7 @@ Route::group(['middleware' => 'myadmin','prefix' => 'myadmin','namespace' => 'My
 
 Route::group(['middleware' => 'myadmin'], function () {
 
-Route::get('/menu/create','MenuController@create');
+Route::get('/menu/create','MenuController@createcurrent');
 Route::get('/menu','MenuController@index');
 Route::get('/menu/del','MenuController@del');
 });
