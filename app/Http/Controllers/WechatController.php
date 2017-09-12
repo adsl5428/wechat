0d30a70c2815ff722f46bdef43492ad55ead40b2
lihongcheng <1519492056@qq.com>
2017-09-12 14:34:24 +0800

    returnkong

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -34,26 +34,26 @@ class WechatController extends Controller
                     }
                     break;
                 case 'text':
-                    return $userApi->get($message->FromUserName)->nickname;
+//                    return $userApi->get($message->FromUserName)->nickname;
                     break;
                 case 'image':
-                    return '收到图片消息';
+//                    return '收到图片消息';
                     break;
                 case 'voice':
-                    return '收到语音消息';
+//                    return '收到语音消息';
                     break;
                 case 'video':
-                    return '收到视频消息';
+//                    return '收到视频消息';
                     break;
                 case 'location':
-                    return '收到坐标消息';
+//                    return '收到坐标消息';
                     break;
                 case 'link':
-                    return '收到链接消息';
+//                    return '收到链接消息';
                     break;
                 // ... 其它消息
                 default:
-                    return '收到其它消息';
+//                    return '收到其它消息';
                     break;
             }
         });
